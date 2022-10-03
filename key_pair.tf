8ee41bf2fa1e36add80472d2d5ad2ddee07ca269
dev/linux <dev@gmail.com>
2022-10-03 12:26:06 +0530

    data commit

--- key_pair.tf
+++ key_pair.tf
@@ -4,3 +4,16 @@ resource "aws_key_pair" "tf-key" {
   public_key = file("${path.module}/id_rsa.pub")
 
 } 
+# resource "tls_private_key" "pk" {
+#   algorithm = "RSA"t
+#   rsa_bits  = 4096
+# }
+
+# resource "aws_key_pair" "kp" {
+#   key_name   = "myKey"       # Create a "myKey" to AWS!!
+#   public_key = tls_private_key.pk.public_key_openssh
+
+#   provisioner "local-exec" { # Create a "myKey.pem" to your computer!!
+#     command = "echo '${tls_private_key.pk.private_key_pem}' > ./myKey1.pem"
+#   }
+# }  
